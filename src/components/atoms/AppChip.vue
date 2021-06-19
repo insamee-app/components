f62dacee555de6df57126a2e15fb5b57f101a62f
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-20 01:47:43 +0200

    feat(chip): rework class and instance

--- src/components/atoms/AppChip.vue
+++ src/components/atoms/AppChip.vue
@@ -1,15 +1,19 @@
 <template>
   <component
     :is="component"
-    class="bg-grey-base text-white text-sm rounded-full px-2 py-1 mb-0 list-none"
+    class="text-white text-sm rounded-full px-2 py-1 mb-0 list-none"
+    :class="classChip"
   >
     <slot></slot>
   </component>
 </template>
 
 <script>
+import vairant from '../../mixins/variant'
+
 export default {
   name: 'AppChip',
+  mixins: [vairant],
   props: {
     component: {
       type: String,
@@ -17,6 +21,29 @@ export default {
       validator(value) {
         return ['li', 'span'].includes(value)
       },
+    },
+    small: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classChip() {
+      const classNames = []
+
+      if (this.isPrimary) {
+        classNames.push('bg-grey-base')
+      } else if (this.isSecondary) {
+        classNames.push('bg-grey-secondary-base')
+      }
+
+      if (this.small) {
+        classNames.push('text-xs')
+      } else {
+        classNames.push('text-sm')
+      }
+
+      return classNames.join(' ')
     },
   },
 }
