584f43049148fbba8cac5178d403a0453d7f6d95
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-20 22:38:55 +0200

    fix(footer): update bg color

--- src/components/organisms/Footer.vue
+++ src/components/organisms/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer class="p-4 text-center text-grey-dark bg-grey-light">
+  <footer class="p-4 text-center grey-dark bg-grey-secondary-light">
     <slot></slot>
     <slot name="by">
       <div :class="$slots.default ? 'mt-6' : undefined">
         Réalisé avec
-        <IconHeart class="w-6 h-6 text-primary-dark fill-current inline" />
+        <IconHeart class="w-6 h-6 text-secondary-base fill-current inline" />
         par des étudiants pour les étudiants
       </div>
     </slot>
