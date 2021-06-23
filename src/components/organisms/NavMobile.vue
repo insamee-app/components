c4d8a488e4845a8e05511d18a2e240df69a77c9b
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 23:17:48 +0200

    fix(NavMobile): update size

--- src/components/organisms/NavMobile.vue
+++ src/components/organisms/NavMobile.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="value" class="fixed top-0 bottom-0 right-0 left-0 z-10 flex flex-row justify-end">
     <div class="absolute w-full h-full" @click="close"></div>
-    <div class="relative h-full w-8/12 bg-white space-y-8">
+    <div class="relative h-full w-9/12 bg-white space-y-8">
       <div class="flex justify-end pt-4 px-4">
         <AppButton empty @click="close">
           <IconDismiss class="text-primary-base h-8 w-8 fill-current" />
