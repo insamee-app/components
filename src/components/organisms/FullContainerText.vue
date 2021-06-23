1dae1e600d6c3efcb8ef54e2c5a1892dc4ec66de
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 22:58:56 +0200

    fix(FullContainerText): add max size

--- src/components/organisms/FullContainerText.vue
+++ src/components/organisms/FullContainerText.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="font-light text-xl md-text-2xl lg:text-3xl px-4">
+  <p class="font-light text-xl md-text-2xl lg:text-3xl px-4 max-w-4xl">
     <slot></slot>
   </p>
 </template>
