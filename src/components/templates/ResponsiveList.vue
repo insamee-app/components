621244ca46ec84eb58d48a079e2254e4f2be0413
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-24 20:13:10 +0200

    style(ResponsiveList): restyle

--- src/components/templates/ResponsiveList.vue
+++ src/components/templates/ResponsiveList.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <AppContainer class="grid grid-cols-1 lg:grid-cols-3 gap-2 lg:gap-8">
-      <div class="lg:row-span-2">
-        <div
-          class="space-y-4 flex flex-row items-center lg:flex-col lg:items-start lg:sticky lg:top-4"
-        >
-          <slot name="filter"></slot>
+    <client-only>
+      <AppContainer class="grid grid-cols-1 lg:grid-cols-3 gap-2 lg:gap-8">
+        <div class="lg:row-span-2">
+          <div
+            class="
+              space-y-4
+              flex flex-row
+              items-center
+              lg:items-start lg:flex-col lg:sticky lg:top-4
+            "
+          >
+            <slot name="filter"></slot>
+          </div>
         </div>
-      </div>
-      <slot name="cards"></slot>
-    </AppContainer>
-    <slot name="modal"></slot>
+        <slot name="cards"></slot>
+      </AppContainer>
+      <slot name="modal"></slot>
+    </client-only>
   </div>
 </template>
 
