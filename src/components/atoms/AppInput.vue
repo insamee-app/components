01858e5bd7cc5fbcd3a1e4748e7d2bd8f47f475f
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-07-01 17:20:02 +0200

    feat(AppInput): create new variants

--- src/components/atoms/AppInput.vue
+++ src/components/atoms/AppInput.vue
@@ -1,26 +1,24 @@
 <template>
-  <input
-    :id="name"
-    :value="value"
-    :type="type"
-    :placeholder="placeholder"
-    class="
-      border border-primary-dark
-      rounded
-      py-1
-      px-2
-      w-full
-      placeholder-grey-base
-      focus:
-      outline-none
-    "
-    @input="$emit('input', $event.target.value)"
-  />
+  <div class="rounded py-1 px-2 w-full" :class="classAppInput">
+    <slot name="prepend"></slot>
+    <input
+      :id="name"
+      :value="value"
+      :type="type"
+      :placeholder="placeholder"
+      class="focus:outline-none w-full"
+      :class="classInput"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script>
+import { variant } from '../../mixins'
+
 export default {
   name: 'AppInput',
+  mixins: [variant],
   props: {
     value: {
       type: [String, Number],
@@ -30,6 +28,10 @@ export default {
       type: String,
       default: '',
     },
+    border: {
+      type: Boolean,
+      default: false,
+    },
     type: {
       type: String,
       default: 'text',
@@ -37,6 +39,36 @@ export default {
     placeholder: {
       type: String,
       default: '',
+    },
+  },
+  computed: {
+    classInput() {
+      const classNames = []
+
+      if (!this.border) {
+        if (this.isPrimary) classNames.push('bg-primary-base')
+        else if (this.isSecondary) classNames.push('bg-secondary-base')
+      }
+
+      if (this.isPrimary) classNames.push('placeholder-grey-light')
+      else if (this.isSecondary) classNames.push('placeholder-grey-secondary-light')
+
+      return classNames.join(' ')
+    },
+    classAppInput() {
+      const classNames = []
+
+      if (this.border) {
+        classNames.push('border text-black')
+        if (this.isPrimary) classNames.push('border-primary-dark')
+        else if (this.isSecondary) classNames.push('border-secondary-dark')
+      } else {
+        classNames.push('text-white')
+        if (this.isPrimary) classNames.push('bg-primary-base')
+        else if (this.isSecondary) classNames.push(' bg-secondary-base')
+      }
+
+      return classNames.join(' ')
     },
   },
 }
--- src/stories/Input.stories.js
+++ src/stories/Input.stories.js
@@ -5,6 +5,11 @@ export default {
   component: AppInput,
   title: 'Atoms/AppInput',
   argTypes: {
+    prepend: {
+      control: 'text',
+      description: 'Slot content',
+      defaultValue: '',
+    },
     input: { action: 'input', table: { type: { summary: 'string' } } },
   },
 }
@@ -15,14 +20,33 @@ const Template = (args, { argTypes }) => ({
   methods: {
     action: action('input'),
   },
-  template: '<AppInput :value="value" @input="action" :type="type" :placeholder="placeholder" />',
+  template:
+    '<AppInput :value="value" @input="action" :type="type" :placeholder="placeholder" :variant="variant" :border="border" />',
 })
 
-export const Default = Template.bind({})
-Default.args = { value: 'Try it!' }
+export const Primary = Template.bind({})
+Primary.args = { value: 'Try it!' }
+
+export const PrimaryBorder = Template.bind({})
+PrimaryBorder.args = { ...Primary.args, border: true }
+
+export const Secondary = Template.bind({})
+Secondary.args = { ...Primary.args, variant: 'secondary' }
+
+export const SecondaryBorder = Template.bind({})
+SecondaryBorder.args = { ...PrimaryBorder.args, variant: 'secondary' }
+
+export const PrimaryPlaceholder = Template.bind({})
+PrimaryPlaceholder.args = { placeholder: 'this is the placeholder' }
+
+export const PrimaryBorderPlaceholder = Template.bind({})
+PrimaryBorderPlaceholder.args = { ...PrimaryPlaceholder.args, border: true }
+
+export const SecondaryPlaceholder = Template.bind({})
+SecondaryPlaceholder.args = { ...PrimaryPlaceholder.args, variant: 'secondary' }
+
+export const SecondaryBorderPlaceholder = Template.bind({})
+SecondaryBorderPlaceholder.args = { ...PrimaryBorderPlaceholder.args, variant: 'secondary' }
 
 export const Number = Template.bind({})
 Number.args = { value: '', type: 'Number' }
-
-export const Placeholder = Template.bind({})
-Placeholder.args = { value: '', placeholder: 'this is a placeholder' }
