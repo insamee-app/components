1e2514201887296464e3e237f7298623963ca540
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-05 01:25:10 +0200

    build: update to pnpm

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,8 +4,8 @@ git push origin develop
 git checkout master
 git merge develop
 git push origin master
-npm run release
+pnpm release
 git checkout develop
 git merge master
 git push origin develop
-yarn format
+pnpm format
