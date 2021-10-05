50b4c91b19104bdb0c60ef13dd240a7bfb65c8c0
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-05 11:52:57 +0200

    fix(scripts): remove unused params

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@ git push origin develop
 git checkout master
 git merge develop
 git push origin master
-pnpm release --no-cleanup
+pnpm release
 git checkout develop
 git merge master
 git push origin develop
